a8c0d97058a2e63f4186da6ffab7486fbe9cbe4a
gareth.evans <gareth.evans@thetrainline.com>
2019-12-19 09:10:47 +0000

    added reflection based function discovery

--- Tests/TestServerHost.cs
+++ Tests/TestServerHost.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Reflection;
 using System.Threading.Tasks;
 using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
@@ -14,10 +13,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -25,7 +24,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using Shouldly;
-using RunExtensions = Microsoft.AspNetCore.Builder.RunExtensions;
 
 namespace Tests
 {
@@ -57,7 +55,6 @@ namespace Tests
     public class TestWebApplicationFactory<TStartup>
         where TStartup : FunctionsStartup, new()
     {
-
         private readonly IWebHostBuilder _webHostBuilder;
 
         public TestWebApplicationFactory()
@@ -71,7 +68,7 @@ namespace Tests
             startup.Configure(functionsHostBuilder);
 
             var loggerMock = new Mock<ILogger>();
-            
+
             _webHostBuilder.ConfigureServices(services =>
             {
                 services
@@ -93,13 +90,29 @@ namespace Tests
             {
                 app.Run(async context =>
                 {
-                    var logger = context.RequestServices.GetRequiredService<ILogger>();
+                    //TODO: function discovery should be done once at start up
+                    var functions = DiscoverFunctions();
 
-                    if (context.Request.Path.StartsWithSegments("/api/alexa"))
+                    foreach (var function in functions)
                     {
-                        var function = context.RequestServices.GetRequiredService<AlexaSkill>();
+                        if (!context.Request.Path.StartsWithSegments($"/api/{function.name}")) continue;
 
-                        var result = await function.Run(context.Request, logger);
+                        var parameters = function.methodInfo.GetParameters();
+
+                        var arguments = parameters.Select(parameter =>
+                        {
+                            if (parameter.ParameterType == typeof(HttpRequest)) return context.Request;
+                            var argument = context.RequestServices.GetService(parameter.ParameterType);
+                            if (argument == null) throw new ArgumentException($"Could not resolve type {parameter.ParameterType} for method {function.methodInfo.DeclaringType}.{function.methodInfo.Name}");
+                            
+                            return argument;
+                        }).ToArray();
+
+                        var target = context.RequestServices.GetService(function.methodInfo.DeclaringType);
+
+                        var invocationTask = (Task<IActionResult>)function.methodInfo.Invoke(target, arguments); //TODO: assuming return type of Task<IActionResult> for now
+
+                        var result = await invocationTask;
 
                         await context.ExecuteResultAsync(result);
 
@@ -113,6 +126,16 @@ namespace Tests
 
         public TestServer CreateServer() => new TestServer(_webHostBuilder);
 
+        private IEnumerable<(string name, MethodInfo methodInfo)> DiscoverFunctions()
+        {
+            var assembly = typeof(TStartup).Assembly; //TODO: we probably want to allow multiple assemblies to used for discovery
+            var allTypes = assembly.GetTypes();
+
+            return allTypes
+                .SelectMany(type => type.GetMethods(BindingFlags.Instance | BindingFlags.Public))
+                .SelectMany(method => method.GetCustomAttributes<FunctionNameAttribute>()
+                    .Select(functionName => (functionName.Name, method)));
+        }
     }
 
     public class TestFunctionsHostBuilder : IFunctionsHostBuilder
