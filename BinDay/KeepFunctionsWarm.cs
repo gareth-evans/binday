bb9692994932d8637b054abc7a0ed136cfad61e1
Gareth Evans <gareth@garethevans.com>
2020-11-25 20:32:23 +0000

    added keep warm function

---
+++ BinDay/KeepFunctionsWarm.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace BinDay
+{
+    public class KeepFunctionsWarm
+    {
+        private readonly ILogger<KeepFunctionsWarm> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        private const string BinDayUrl = "https://binday-production.azurewebsites.net/api/alexa";
+
+        public KeepFunctionsWarm(ILogger<KeepFunctionsWarm> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [FunctionName("KeepWarm")]
+        public async Task Run([TimerTrigger("0 */10 * * * *")] TimerInfo myTimer)
+        {
+            using var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.PostAsync(BinDayUrl, null);
+
+            _logger.LogInformation($"Got {response.StatusCode} from {BinDayUrl}");
+        }
+    }
+}
